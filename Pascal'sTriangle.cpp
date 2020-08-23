09c41e2ea1dff036cf9599b3a9640d9911559a13
Anusha R <53563621+AnushaR2416@users.noreply.github.com>
2020-08-23 13:03:17 +0530

    Create Pascal'sTriangle.cpp

---
+++ Pascal'sTriangle.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cin>>n;
+  int num=1;
+  for(int i=1;i<=n;i++)
+  {
+      num=1;
+      for(int j=i;j<=i;j++)
+      {
+        cout<<num<<" ";
+        num=num*(i-j)/j;
+      }
+      cout<<endl;
+  }
+  return 0;
+}
